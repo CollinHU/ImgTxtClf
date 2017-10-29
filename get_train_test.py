6f0c2595017c04a5ae04791ee21e0ccd8004b808
Ubuntu <collin@collin.vor24gn0kikerpeqvmntk2sjyb.ix.internal.cloudapp.net>
2017-10-29 09:46:28 +0000

    train /test dataset

--- get_train_test.py
+++ get_train_test.py
@@ -1,36 +1,91 @@
 import pandas as pd
 
-data = pd.read_csv('../upmc_food_101_df.csv',index_col = 0)
+#path = '../umpc_food_101_df.csv'
+def train_test_data_set():
+    path = '../upmc_food_101_df.csv'
+    data = pd.read_csv(path,index_col = 0)
+    
+    category_name = data['category'].unique()
+    categories = {}
+    size = len(category_name)
 
-category_name = data['category'].unique()
-categories = {}
-df_list = {'category':[],'recipe':[],'category_id':[]}
-size = len(category_name)
-for i in range(size):
-    categories[category_name[i]] = i
+    for i in range(size):
+        categories[category_name[i]] = i
+    
+    category_id = pd.DataFrame(data = categories,index=range(1))
+    data['category_id'] = [categories[item] for item in data['category'].values]
+    
+    
+    df_list = {'category':[],'recipe':[],'category_id':[]}
+    test_df = pd.DataFrame(data = df_list)
+    for i in range(size):
+        test_df =  test_df.append(data[data['category_id']==i].sample(frac = 0.2))
+        
+    test_df = test_df[['category','category_id','recipe']]
+    data = data[['category','category_id','recipe']]
+    test_index = test_df.index.values
+    train_df = data.drop(test_index)
 
-data['category_id'] = [categories[item] for item in data['category'].values]
+    test_df = test_df.sample(frac = 1)
+    train_df = train_df.sample(frac = 1)
+
+#    print(train_df.head())
+    train_df.reset_index(inplace = True)
+    train_df.drop('index',axis = 1,inplace =True)
+    test_df.reset_index(inplace = True)
+    test_df.drop('index',axis = 1, inplace=True)
+    train_size = len(train_df.index)
+    test_size = len(test_df.index)
+    print 'there are {} samples in train dataset\n'.format(train_size)
+ #   print(train_df.head())
+    print 'there are {} samples in test dataset\n'.format(test_size)
+  #  print(test_df.head())
+
+    train_df.to_csv('../train_df.csv')
+    test_df.to_csv('../test_df.csv')
+    category_id.to_csv('../category_id.csv')
+
+    print('finish test/train data loading\n')
+
+def load_data_set():
+    try:
+        test_df = pd.read_csv('../test_df.csv')
+        train_df = pd.read_csv('../train_df.csv')
+        categories = pd.read_csv('../category_id.csv')
+    except:
+        print("Creating train/test data\n")
+        train_test_data_set()
+        test_df = pd.read_csv('../test_df.csv')
+        train_df = pd.read_csv('../train_df.csv')
+        categories = pd.read_csv('../category_id.csv')
+    
+    data_dic = {}
+    
+    category_id = {}
+    for col in categories.columns.values:
+        category_id[col] = categories[col].values[0]
+    
+    train = {}
+    train['data'] = list(train_df['recipe'].values)
+    train['target'] = train_df['category_id'].values
+
+    test = {}
+    test['data'] = list(test_df['recipe'].values)
+    test['target'] = test_df['category_id'].values
 
 
-test_df = pd.DataFrame(data = df_list)
-for i in range(size):
-   test_df =  test_df.append(data[data['category_id']==i].sample(frac = 0.2))
-
-test_df = test_df[['category','category_id','recipe']]
-data = data[['category','category_id','recipe']]
-
-test_index = test_df.index.values
-train_df = data.drop(test_index)
-train_df.to_csv('train_df.csv')
-test_df.to_csv('test_df.csv')
+    data_dic['categories'] = category_id
+    data_dic['train'] = train
+    data_dic['test'] = test
+    return data_dic
 #print(size)
-print "\n"
-print(test_df.count())
+#print "\n"
+'''print(test_df.count())
 print(train_df.count())
 print(test_df.head())
-print(train_df.head())
+print(train_df.head())'''
 #print(type(data['category'].values))
 #print data.head(5)
 #print(categories)
 
-
+load_data_set()
