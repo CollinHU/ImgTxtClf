fd67a83ad589b157bd67add2f80cba5a412eba56
Ubuntu <collin@collin.vor24gn0kikerpeqvmntk2sjyb.ix.internal.cloudapp.net>
2017-11-14 12:09:18 +0000

    I went to the woods because I wished to live deliberately, to front only the essential facts of life, and see if I could not learn what it had to teach, and not, when I came to die, discover that I had not lived. I did not wish to live what was not life, living is so dear; nor did I wish to practise resignation, unless it was quite necessary. I wanted to live deep and suck out all the marrow of life, to live so sturdily and Spartan-like as to put to rout all that was not life, to cut a broad swath and shave close, to drive life into a corner, and reduce it to its lowest terms...

--- text_prcoess/SVM_clf/svm_common_text_clf.py
+++ text_prcoess/SVM_clf/svm_common_text_clf.py
@@ -7,16 +7,16 @@ import gc
 
 print('reading data')
 X_train = pd.read_csv('/mnt/zyhu/common/texts/train_tfidf_w2v_df.csv',index_col = 0)
-X_train_data = X_train.iloc[:,:100].values
+X_train_data = X_train.iloc[:,:200].values
 #X_test_data = pickle.load(open('../data/test_data_ngram_1.pkl','rb'))
 
 X_val = pd.read_csv('/mnt/zyhu/common/texts/val_tfidf_w2v_df.csv',index_col = 0)
-X_val_data = X_val.iloc[:,:100].values
+X_val_data = X_val.iloc[:,:200].values
 
 X_train_data = np.append(X_train_data, X_val_data, axis = 0)
 
 X_test = pd.read_csv('/mnt/zyhu/common/texts/test_tfidf_w2v_df.csv',index_col = 0)
-X_test_data = X_test.iloc[:,:100].values
+X_test_data = X_test.iloc[:,:200].values
 
 Y_train = X_train['category_id'].values
 Y_val = X_val['category_id'].values
@@ -31,7 +31,7 @@ def train_test_model(kernel):
     svm_predicted = svm_text_clf.predict(X_train_data)
     svm_accuracy = np.mean(svm_predicted == Y_train)
     
-    record = open('svm_text_clf_record.txt','a')
+    record = open('svm_common_text_clf_record_02.txt','a')
     record.write('using svm  with setting: \n')
     record.write('kernel = {} '.format(kernel))
     record.write('training data the accuracy is: ' + str(svm_accuracy)+'\n')
@@ -43,8 +43,9 @@ def train_test_model(kernel):
     record.close()
 
 print('train and test model')
-kernel_list = ['rbf', 'linear','sigmoid']#,'poly']
+kernel_list = ['sigmoid','rbf','linear'] #,'poly']
 for item in kernel_list:
+    print(item)
     train_test_model(item)
     gc.collect()
 
--- text_prcoess/SVM_clf/svm_text_clf.py
+++ text_prcoess/SVM_clf/svm_text_clf.py
@@ -1,3 +1,5 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import get_train_test as dataset
 from sklearn.svm import SVC
 import pandas as pd
@@ -14,10 +16,10 @@ print(X_train["target"][:3])
 X_test = X_data['test']
 
 print('reading data')
-X_train_data = pd.read_csv('../data/train_tfidf_w2v_df.csv',index_col = 0)
+X_train_data = pd.read_csv('/mnt/zyhu/data/train_tfidf_w2v_df.csv',index_col = 0)
 X_train_data = X_train_data.values
 #X_test_data = pickle.load(open('../data/test_data_ngram_1.pkl','rb'))
-X_test_data = pd.read_csv('../data/test_tfidf_w2v_df.csv',index_col = 0)
+X_test_data = pd.read_csv('/mnt/zyhu/data/test_tfidf_w2v_df.csv',index_col = 0)
 X_test_data = X_test_data.values
 
 Y_train = X_train['target']
@@ -25,12 +27,14 @@ Y_test = X_test['target']
 
 def train_test_model(kernel):
     svm_text_clf = SVC(kernel = kernel)
+    print("training")
     svm_text_clf = svm_text_clf.fit(X_train_data, Y_train)
 
+    print("do predcition")
     svm_predicted = svm_text_clf.predict(X_train_data)
     svm_accuracy = np.mean(svm_predicted == Y_train)
     
-    record = open('svm_text_clf_record.txt','a')
+    record = open('testing_speed.txt','a')
     record.write('using svm  with setting: \n')
     record.write('kernel = {} '.format(kernel))
     record.write('training data the accuracy is: ' + str(svm_accuracy)+'\n')
@@ -42,8 +46,9 @@ def train_test_model(kernel):
     record.close()
 
 print('train and test model')
-kernel_list = ['rbf', 'linear','sigmoid','poly']
+kernel_list = ['rbf', 'linear','sigmoid']#,'poly']
 for item in kernel_list:
+    print(item)
     train_test_model(item)
     gc.collect()
 
